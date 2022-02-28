d14d4c7b2b1df792f979b267e94c851a52e3241a
Gordon Freeman <drfr33man@protonmail.com>
2022-02-28 23:25:03 +0200

    install electron-builder

--- electron-react-demo/src/App.js
+++ electron-react-demo/src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { withRouter } from "./withRouter";
 import { useNavigate, Outlet } from "react-router";
-import { Provider, connect } from 'react-redux'
+import { Provider, connect } from "react-redux";
 
 import TitleBar from "./Components/TitleBar";
 import { Component } from "react";
 import PropTypes from "prop-types";
 import { loadUser } from "./actions/authAction";
 import { returnErrors, clearErrors } from "./actions/errorAction";
+import { LOGIN_FAIL, RESET_FAIL, AUTH_ERROR } from "./actions/types";
 class App extends Component {
   static propTypes = {
     isAuthenticated: PropTypes.bool,
@@ -22,35 +23,30 @@ class App extends Component {
   componentWillMount() {
     this.props.loadUser();
     //console.log("AppWillMount")
-
   }
 
   componentDidUpdate(prevProps, prevState) {
-
     const error = this.props.error;
     console.log(error);
     // if (error !== prevProps.error) {
 
-    if (error.id === "AUTH_ERROR") {
-      this.props.navigate('/Login')
+    if (
+      error.id === AUTH_ERROR ||
+      error.id === LOGIN_FAIL ||
+      error.id === RESET_FAIL
+    ) {
+      this.props.navigate("/Login");
     }
 
-
     this.props.navigate("/Main");
-
   }
-
-
-
 
   render() {
     return (
-
       <div className="App ">
         {/* <TitleBar /> */}
         <Outlet />
       </div>
-
     );
   }
 }
@@ -61,7 +57,6 @@ const mapStateToProps = state => ({
   error: state.error,
   user: state.auth.user
 });
-
 
 export default withRouter(
   connect(mapStateToProps, { loadUser, returnErrors, clearErrors })(App)
