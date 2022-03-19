1b12df25b961925908bdc05cbf54f7253cba2929
Gordon Freeman <drfr33man@protonmail.com>
2022-03-19 05:21:11 +0200

    fix add route

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,104 +1,103 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-
+const bcryptjs = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
 
-
 router.post("/", async (req, res) => {
+  try {
+    employees = await User.findAll({
+      raw: true,
+      nest: true
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-    try {
-        employees = await User.findAll({
-
-            raw: true,
-            nest: true
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
-
-    res.json(employees);
+  res.json(employees);
 });
 
 // @route POST api/users
 // @desc Register New User
 // @acces Public
-router.post("/add", async (req, res) => {
-    console.log('add Employee called');
-    const { name, phone, password, type, nationality } = req.body;
-    // Verify URL
-    // const query = stringify({
-    //   secret: config.get("reCAPTCHA"),
-    //   response: req.body.token,
-    //   remoteip: req.connection.remoteAddress
-    // });
-    // const verifyURL = `${config.get("verifyURL")}${query}`;
-    // //console.log(verifyURL);
-    // const body = await axios.get(verifyURL);
-    //console.log(body.data);
-    // if (body.data.success !== undefined && !body.data.success) {
-    //   return res.status(400).json({ msg: "Failed captcha verification" });
-    // }
-    if (req.user.isManager !== 'manager') {
-        return res.status(400).json({ msg: "Not allowed to manage employees" });
-    }
+router.post("/add", auth, async (req, res) => {
+  console.log("add Employee called");
+  const { name, phone, password, type, nationality } = req.body;
+  // Verify URL
+  // const query = stringify({
+  //   secret: config.get("reCAPTCHA"),
+  //   response: req.body.token,
+  //   remoteip: req.connection.remoteAddress
+  // });
+  // const verifyURL = `${config.get("verifyURL")}${query}`;
+  // //console.log(verifyURL);
+  // const body = await axios.get(verifyURL);
+  //console.log(body.data);
+  // if (body.data.success !== undefined && !body.data.success) {
+  //   return res.status(400).json({ msg: "Failed captcha verification" });
+  // }
+  if (req.user.isManager !== true) {
+    return res.status(400).json({ msg: "Not allowed to manage employees" });
+  }
 
-    if (!name || !phone || !password || !type || !nationality) {
-        return res.status(400).json({ msg: "Please enter all fields" });
-    }
+  if (!name || !phone || !password || !type || !nationality) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
 
-    // Check for exitsting user
-    let user = await User.findOne(
-        { where: { phone: `${phone}` } },
-        { plain: true }
-    );
-    if (user) {
-        return res.status(400).json({ msg: "User alerady exists." });
-    }
+  // Check for exitsting user
+  let user = await User.findOne(
+    { where: { phone: `${phone}` } },
+    { plain: true }
+  );
+  if (user) {
+    return res.status(400).json({ msg: "User alerady exists." });
+  }
 
-    // const newUser = new User({
-    //   name,
-    //   email,
-    //   password
-    // });
-    const newUser = User.build({
-        name: `${name}`,
-        phone: `${phone}`,
-        password: `${password}`,
-        country: `${nationality}`,
-        isManager: type === 'manager'
-    });
+  // const newUser = new User({
+  //   name,
+  //   email,
+  //   password
+  // });
+  const newUser = User.build({
+    name: `${name}`,
+    phone: `${phone}`,
+    password: `${password}`,
+    country: `${nationality}`,
+    isManager: type === "manager",
+    active: true
+  });
 
-    // Create salt and hash
+  // Create salt and hash
 
-    bcryptjs.genSalt(10, (err, salt) => {
-        bcryptjs.hash(newUser.password, salt, (err, hash) => {
+  bcryptjs.genSalt(10, (err, salt) => {
+    bcryptjs.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save().then(user => {
+        jwt.sign(
+          { id: user.id },
+          config.get("jwtSecret"),
+          {
+            expiresIn: 604800
+          },
+          (err, token) => {
             if (err) throw err;
-            newUser.password = hash;
-            newUser.save().then(user => {
-                jwt.sign(
-                    { id: user.id },
-                    config.get("jwtSecret"),
-                    {
-                        expiresIn: 604800
-                    },
-                    (err, token) => {
-                        if (err) throw err;
-                        res.json({
-                            token,
-                            user: {
-                                id: user.id,
-                                name: user.name,
-                                phone: user.phone,
-                                active: user.active
-                            }
-                        });
-                    }
-                );
+            res.json({
+              token,
+              user: {
+                id: user.id,
+                name: user.name,
+                phone: user.phone,
+                active: user.active
+              }
             });
-        });
+          }
+        );
+      });
     });
+  });
 });
 
 module.exports = router;
